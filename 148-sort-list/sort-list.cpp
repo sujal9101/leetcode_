9f3cfacc861f2311403ab440a7b56186580ffc95
Sujal Gangshettiwar <126855477+sujal9101@users.noreply.github.com>
2024-09-05 17:35:58 +0530

    Time: 120 ms (78.29%) | Memory: 55.8 MB (95.22%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -6,24 +6,19 @@ public:
         
         ListNode* slow = head;
         ListNode* fast = head->next;
-    
         while (fast != NULL && fast->next != NULL)
         {
             slow = slow->next;
             fast = fast->next->next;
         }
-        //divide the list into two parts 
         fast = slow->next;
         slow->next = NULL;
-        
         return merge(sortList(head), sortList(fast));
     }
-    
     ListNode* merge(ListNode* l1, ListNode* l2)
     {
         ListNode dump(0);
         ListNode* cur = &dump;
-        
         while (l1 != NULL && l2 != NULL)
         {
             if (l1->val < l2->val)
@@ -38,8 +33,7 @@ public:
             }
                 
             cur = cur->next;
-        }
-        
+        }        
         if (l1 != NULL)
             cur->next = l1;
         else
