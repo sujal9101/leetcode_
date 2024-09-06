d1ae0742cac2447e97fa8529cf9feac6ee6e8394
Sujal Gangshettiwar <126855477+sujal9101@users.noreply.github.com>
2024-09-06 17:26:34 +0530

    Time: 89 ms (37.34%) | Memory: 114.5 MB (63.19%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -7,11 +7,11 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]<=first_small)
-                first_small=nums[i];
+                first_small=nums[i];  //Update the smallest number found so far
             else if(nums[i]<= second_small)
-                second_small=nums[i];
+                second_small=nums[i]; // Update the second smallest number
             else
-                return true;
+                return true; //found a number greater than first_small and second_small
         }
         return false;
     }
