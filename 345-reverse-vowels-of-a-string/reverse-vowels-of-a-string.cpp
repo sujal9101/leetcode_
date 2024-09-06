3c058d19cc6be7a68eccdcf7fc5c170349d2ac2b
Sujal Gangshettiwar <126855477+sujal9101@users.noreply.github.com>
2024-09-06 16:07:38 +0530

    Time: 10 ms (29.91%) | Memory: 9.3 MB (76.99%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -5,11 +5,9 @@ public:
         int n=s.length();
         int i = 0, j = n-1;
         while (i < j) {
-            // Move i forward if s[i] is not a vowel
             while(i<j && vowels.find(s[i]) == string::npos) {
                 i++;
-            }           
-            // Move j backward if s[j] is not a vowel
+            }      
             while(i<j && vowels.find(s[j]) == string::npos) {
                 j--;
             }
